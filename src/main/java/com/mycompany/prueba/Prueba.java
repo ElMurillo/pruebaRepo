4c29905eb3f87c9d95010f35dd018b3073619e51
Muril <Muril@10.234.172.38>
2024-03-18 17:05:12 -0600

    Reemplazo1

--- src/main/java/com/mycompany/prueba/Prueba.java
+++ src/main/java/com/mycompany/prueba/Prueba.java
@@ -9,8 +9,36 @@ package com.mycompany.prueba;
  * @author Muril
  */
 public class Prueba {
+    
+    
+    private int perro;
+    private String gaviota;
 
-    public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+    public Prueba(int perro, String gaviota) {
+        this.perro = perro;
+        this.gaviota = gaviota;
     }
+
+    public int getPerro() {
+        return perro;
+    }
+
+    public void setPerro(int perro) {
+        this.perro = perro;
+    }
+
+    public String getGaviota() {
+        return gaviota;
+    }
+
+    public void setGaviota(String gaviota) {
+        this.gaviota = gaviota;
+    }
+
+    @Override
+    public String toString() {
+        return "Prueba{" + "perro=" + perro + ", gaviota=" + gaviota + '}';
+    }
+    
+    
 }
